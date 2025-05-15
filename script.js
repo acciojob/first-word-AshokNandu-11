4915071b8b0b0018d9d511758cc8ee42a34db3f8
AshokNandu-11 <114278147+AshokNandu-11@users.noreply.github.com>
2025-05-15 13:54:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-function firstWord(s) {
+function firstWord(str) {
   // your code here
-	let trimstar = s.trim();
-	let words = s.split(" ");
+	let trimStr = str.trim();
+  // Split the string into an array of words based on space character
+	let words = str.split(" "); // ["see", "and", "stop"]
+  // Return the first word (first element of the array)
 	return words[0];
+}
 }
 
 // Do not change the code below
